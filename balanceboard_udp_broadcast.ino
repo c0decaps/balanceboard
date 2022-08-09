8b2d038d973918007347352657096a50b3e0e42f
c0decaps <manuel.weiss@protonmail.ch>
2022-08-09 14:06:03 +0200

    added esp8266 compatibility

--- balanceboard_udp_broadcast.ino
+++ balanceboard_udp_broadcast.ino
@@ -1,9 +1,14 @@
+#if defined(ESP32)
+#include <ESPmDNS.h>
 #include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266mDNS.h>
+#include <ESP8266WiFi.h>
+#endif
 #include <WiFiUdp.h>
 #include "Wire.h"
 #include <MPU6050_light.h>
 #include <WiFiManager.h>
-#include <ESPmDNS.h>
 #define TRIGGER_PIN 0
 
 IPAddress udpAddress;
